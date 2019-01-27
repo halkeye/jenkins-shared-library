584f1af05e8fb7fdf48c5a5080c8367dc4623d0c
Gavin Mogan <git@gavinmogan.com>
2019-01-26 21:10:12 -0800

    Config by body

--- vars/buildHelmChart.groovy
+++ vars/buildHelmChart.groovy
@@ -1,4 +1,10 @@
-def call() {
+def call(body) {
+  // evaluate the body block, and collect configuration into the object
+  def pipelineParams= [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
+
   ansiColor('xterm') {
     timeout(10) {
       dir('chart') {
