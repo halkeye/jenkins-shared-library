ec3b672eb1ebe5319ec4d6d7eb2590717045f90b
Gavin Mogan <git@gavinmogan.com>
2019-01-26 21:12:30 -0800

    if no body, ignore trying to get parameters

--- vars/buildHelmChart.groovy
+++ vars/buildHelmChart.groovy
@@ -1,9 +1,11 @@
 def call(body) {
   // evaluate the body block, and collect configuration into the object
   def pipelineParams= [:]
-  body.resolveStrategy = Closure.DELEGATE_FIRST
-  body.delegate = pipelineParams
-  body()
+  if (body) {
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+  }
 
   ansiColor('xterm') {
     timeout(10) {
