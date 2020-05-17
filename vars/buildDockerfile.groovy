877d5cdbf27e6a1ff17b405a398cf9feda25b506
Gavin Mogan <git@gavinmogan.com>
2020-05-17 12:31:08 -0700

    all all the labels

--- vars/buildDockerfile.groovy
+++ vars/buildDockerfile.groovy
@@ -15,7 +15,23 @@ def call(imageName, registry = "", credential = "dockerhub-halkeye") {
         steps {
           sh "docker login --username=\"$DOCKER_USR\" --password=\"$DOCKER_PSW\" ${registry}"
           sh "docker pull ${registry}${imageName} || true"
-          sh "docker build -t ${registry}${imageName} ."
+          GIT_COMMIT_REV = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
+          GIT_SCM_URL = sh(returnStdout: true, script: "git remote show origin | grep 'Fetch URL' | awk '{print $3}'").trim()
+          SCM_URI = GIT_SCM_URL.replace("git@github.com:", "https://github.com/");
+          BUILD_DATE = sh(returnStdout: true, script: "TZ=UTC date --rfc-3339=seconds | sed 's/ /T/'").trim()
+          sh """
+            docker build \
+              -t ${registry}${imageName}
+              -l "org.opencontainers.image.source=${GIT_SCM_URL}" \
+              -l "org.label-schema.vcs-url=${GIT_SCM_URL}" \
+              -l "org.opencontainers.image.url==${SCM_URI}" \
+              -l "org.label-schema.url=${SCM_URI}" \
+              -l "org.opencontainers.image.revision=${GIT_COMMIT_REV}" \
+              -l "org.label-schema.vcs-ref=${GIT_COMMIT_REV}" \
+              -l "org.opencontainers.created=${BUILD_DATE}" \
+              -l "org.label-schema.build-date=${BUILD_DATE}" \
+              .
+          """
         }
       }
       stage("Deploy master as latest") {
