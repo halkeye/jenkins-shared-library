fb9253c72cee89c57ab4d9155a11a22cad383b41
Gavin Mogan <git@gavinmogan.com>
2020-10-27 21:19:35 -0700

    mock build plugin for my local testing

---
+++ vars/buildPlugin.groovy
@@ -0,0 +1,15 @@
+#!/usr/bin/env groovy
+def call(Map params = [:]) {
+  pipeline {
+    agent { docker 'maven' }
+    stages {
+      stage('build') {
+        steps {
+          sh ('mvn package')
+          archiveArtifacts artifacts: '**/target/*.hpi,**/target/*.jpi,**/target/*.jar', fingerprint: true
+          junit('**/target/surefire-reports/**/*.xml,**/target/failsafe-reports/**/*.xml,**/target/invoker-reports/**/*.xml')
+        }
+      }
+    }
+  }
+}
