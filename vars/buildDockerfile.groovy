a9bc56d946a8dff0c646d81653d35bde078a9c37
Gavin Mogan <git@gavinmogan.com>
2021-03-11 16:59:01 -0800

    lets fully img

--- vars/buildDockerfile.groovy
+++ vars/buildDockerfile.groovy
@@ -47,31 +47,33 @@ def call(String imageName, Map config=[:], Closure body={}) {
       }
       stage("Build") {
         steps {
-          docker.image(config.buildContainer).inside('--security-opt seccomp=unconfined --security-opt apparmor=unconfined') {
-            sh('''
-              export GIT_COMMIT_REV=$(git log -n 1 --pretty=format:'%h')
-              export GIT_SCM_URL=$(git remote show origin | grep 'Fetch URL' | awk '{print $3}')
-              export SCM_URI=$(echo $GIT_SCM_URL | awk '{print gensub("git@github.com:","https://github.com/",$3)}')
+          script {
+            docker.image(config.buildContainer).inside('--security-opt seccomp=unconfined --security-opt apparmor=unconfined') {
+              sh('''
+                export GIT_COMMIT_REV=$(git log -n 1 --pretty=format:'%h')
+                export GIT_SCM_URL=$(git remote show origin | grep 'Fetch URL' | awk '{print $3}')
+                export SCM_URI=$(echo $GIT_SCM_URL | awk '{print gensub("git@github.com:","https://github.com/",$3)}')
 
-              img version
-              img login --username="$DOCKER_USR" --password="$DOCKER_PSW" $DOCKER_REGISTRY
-              img pull ${IMAGE_NAME} || true
-              img build \
-                  -t ${IMAGE_NAME} \
-                  --build-arg "GIT_COMMIT_REV=$GIT_COMMIT_REV" \
-                  --build-arg "GIT_SCM_URL=$GIT_SCM_URL" \
-                  --build-arg "BUILD_DATE=$BUILD_DATE" \
-                  --label "org.opencontainers.image.source=$GIT_SCM_URL" \
-                  --label "org.label-schema.vcs-url=$GIT_SCM_URL" \
-                  --label "org.opencontainers.image.url=$SCM_URI" \
-                  --label "org.label-schema.url=$SCM_URI" \
-                  --label "org.opencontainers.image.revision=$GIT_COMMIT_REV" \
-                  --label "org.label-schema.vcs-ref=$GIT_COMMIT_REV" \
-                  --label "org.opencontainers.image.created=$BUILD_DATE" \
-                  --label "org.label-schema.build-date=$BUILD_DATE" \
-                  -f ${DOCKERFILE} \
-                  .
-            ''')
+                img version
+                img login --username="$DOCKER_USR" --password="$DOCKER_PSW" $DOCKER_REGISTRY
+                img pull ${IMAGE_NAME} || true
+                img build \
+                    -t ${IMAGE_NAME} \
+                    --build-arg "GIT_COMMIT_REV=$GIT_COMMIT_REV" \
+                    --build-arg "GIT_SCM_URL=$GIT_SCM_URL" \
+                    --build-arg "BUILD_DATE=$BUILD_DATE" \
+                    --label "org.opencontainers.image.source=$GIT_SCM_URL" \
+                    --label "org.label-schema.vcs-url=$GIT_SCM_URL" \
+                    --label "org.opencontainers.image.url=$SCM_URI" \
+                    --label "org.label-schema.url=$SCM_URI" \
+                    --label "org.opencontainers.image.revision=$GIT_COMMIT_REV" \
+                    --label "org.label-schema.vcs-ref=$GIT_COMMIT_REV" \
+                    --label "org.opencontainers.image.created=$BUILD_DATE" \
+                    --label "org.label-schema.build-date=$BUILD_DATE" \
+                    -f ${DOCKERFILE} \
+                    .
+              ''')
+            }
           }
         }
       }
@@ -79,15 +81,15 @@ def call(String imageName, Map config=[:], Closure body={}) {
         when { branch "master" }
         environment { DOCKER = credentials("dockerhub-halkeye") }
         steps {
-          docker.image(config.buildContainer).inside('--security-opt seccomp=unconfined --security-opt apparmor=unconfined') {
-            sh('''
-            img login --username="$DOCKER_USR" --password="$DOCKER_PSW" $DOCKER_REGISTRY
-            img tag $IMAGE_NAME $IMAGE_NAME:${SHORT_GIT_COMMIT_REV}
-            img push $IMAGE_NAME:${SHORT_GIT_COMMIT_REV}
-            img push $IMAGE_NAME
-            ''')
-          }
           script {
+            docker.image(config.buildContainer).inside('--security-opt seccomp=unconfined --security-opt apparmor=unconfined') {
+              sh('''
+              img login --username="$DOCKER_USR" --password="$DOCKER_PSW" $DOCKER_REGISTRY
+              img tag $IMAGE_NAME $IMAGE_NAME:${SHORT_GIT_COMMIT_REV}
+              img push $IMAGE_NAME:${SHORT_GIT_COMMIT_REV}
+              img push $IMAGE_NAME
+              ''')
+            }
             if (currentBuild.description) {
               currentBuild.description = currentBuild.description + " / "
             }
@@ -99,14 +101,14 @@ def call(String imageName, Map config=[:], Closure body={}) {
         when { buildingTag() }
         environment { DOCKER = credentials("dockerhub-halkeye") }
         steps {
-          docker.image(config.buildContainer).inside('--security-opt seccomp=unconfined --security-opt apparmor=unconfined') {
-            sh('''
-            img login --username="$DOCKER_USR" --password="$DOCKER_PSW" $DOCKER_REGISTRY
-            img tag $IMAGE_NAME $IMAGE_NAME:${TAG_NAME}
-            img push $IMAGE_NAME:${TAG_NAME}
-            ''')
-          }
           script {
+            docker.image(config.buildContainer).inside('--security-opt seccomp=unconfined --security-opt apparmor=unconfined') {
+              sh('''
+              img login --username="$DOCKER_USR" --password="$DOCKER_PSW" $DOCKER_REGISTRY
+              img tag $IMAGE_NAME $IMAGE_NAME:${TAG_NAME}
+              img push $IMAGE_NAME:${TAG_NAME}
+              ''')
+            }
             if (currentBuild.description) {
               currentBuild.description = currentBuild.description + " / "
             }
